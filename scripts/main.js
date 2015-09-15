3a2a20ed8a594b32c8173ba614ad32424f2c8b69
Pierre Laferriere <polaferriere@gmail.com>
2015-09-15 19:16:29 -0400

    Created heading and sidebar structure.  Created result grid structure.  Wrote javascript to autopopulate result grid.

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+/*
+  (url: String, callback: Function) -> undefined
+
+  Execute a callback function with the JSON results from the url specified.
+
+  Examples
+      var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=tacos&includes=Images,Shop";
+
+      fetchJSONP(url, function(data) {
+        // do something with data
+      });
+
+      // OR
+
+      function logData(data) {
+        console.log(data);
+      }
+
+      fetchJSONP(url, logData);
+*/
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
+
+var videoGameURL = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=video+games&includes=Images,Shop";
+
+fetchJSONP(videoGameURL, function(response) {
+  var itemResults = response.results;
+  itemResults.forEach(displayResult);
+});
+
+function displayResult(result) {
+  var source = document.querySelector('#result-template').innerHTML;
+
+  var template = Handlebars.compile(source);
+  var outputHTML = template(result);
+
+  var resultsUL = document.querySelector('.results');
+  resultsUL.insertAdjacentHTML('beforeend', outputHTML);
+}
